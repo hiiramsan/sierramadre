10950368019f38c021570057de0790ab620ef33d
Carlos Hiram Sánchez Meneses <carloshiramsanchez@gmail.com>
2026-03-06 17:53:29 -0700

    hola

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
